99f6483efacd7e531104a4efe7544fffa853f8f7
docktermj <michael@dockter.com>
2022-07-08 09:00:34 -0400

    #36 Initial draft

--- rootfs/app/wait-and-run-postgres-client.sh
+++ rootfs/app/wait-and-run-postgres-client.sh
@@ -2,24 +2,27 @@
 
 # Wait until the database is ready.
 
-until pg_isready --dbname=${SENZING_DATABASE_URL}
-do
-    echo "Sleeping 1 second"
-    sleep 1
-done
+#until pg_isready --dbname=${SENZING_DATABASE_URL}
+#do
+#    echo "Sleeping 1 second"
+#    sleep 1
+#done
 
-# If specified, process single SENZING_SQL_FILE
+# If specified, process single SENZING_SQL_FILE.
 
-if [ -n ${SENZING_SQL_FILE} ]; then
-    echo ">>>>> Single: ${SENZING_SQL_FILE}"
+if [ -n "${SENZING_SQL_FILE}" ]; then
+    echo "Processing: ${SENZING_SQL_FILE}"
 #   psql ${SENZING_DATABASE_URL} --file ${SENZING_SQL_FILE}
 fi
 
+# If specified, process multiple SENZING_SQL_FILES.
 
-if [ -n ${SENZING_SQL_FILES} ]; then
-    for SENZING_SQL_FILE in ${SENZING_SQL_FILES[@]}
-    do
-        echo ">>>>> Multiple: ${SENZING_SQL_FILE}"
+if [ -n "${SENZING_SQL_FILES}" ]; then
+    IFS=" "
+    for SENZING_SQL_FILE in ${SENZING_SQL_FILES}; do
+        echo "Processing: ${SENZING_SQL_FILE}"
 #       psql ${SENZING_DATABASE_URL} --file ${SENZING_SQL_FILE}
     done
 fi
+
+echo "Completed."

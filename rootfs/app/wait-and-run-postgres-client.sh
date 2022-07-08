7275b0ccb1b1b6700bc01a4182aa2d2492d65e62
docktermj <michael@dockter.com>
2022-07-08 08:29:36 -0400

    #36 Initial draft

--- rootfs/app/wait-and-run-postgres-client.sh
+++ rootfs/app/wait-and-run-postgres-client.sh
@@ -4,10 +4,22 @@
 
 until pg_isready --dbname=${SENZING_DATABASE_URL}
 do
-   echo "Sleeping 1 second"
-   sleep 1
+    echo "Sleeping 1 second"
+    sleep 1
 done
 
-# Process the file specified by SENZING_SQL_FILE
+# If specified, process single SENZING_SQL_FILE
 
-psql ${SENZING_DATABASE_URL} --file ${SENZING_SQL_FILE}
+if [ -n ${SENZING_SQL_FILE} ]; then
+    echo ">>>>> Single: ${SENZING_SQL_FILE}"
+#   psql ${SENZING_DATABASE_URL} --file ${SENZING_SQL_FILE}
+fi
+
+
+if [ -n ${SENZING_SQL_FILES} ]; then
+    for SENZING_SQL_FILE in ${SENZING_SQL_FILES[@]}
+    do
+        echo ">>>>> Multiple: ${SENZING_SQL_FILE}"
+#       psql ${SENZING_DATABASE_URL} --file ${SENZING_SQL_FILE}
+    done
+fi
